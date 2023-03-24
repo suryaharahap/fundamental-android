acce14d075d096e4ef472527c9c3cb751e781fa9
suryaharahap <suryaharahap18@gmail.com>
2023-03-24 17:18:10 +0700

    Add: circleImageView and glide

--- MyNavigationDrawer/app/src/main/java/me/suryaharahap/mynavigationdrawer/MainActivity.kt
+++ MyNavigationDrawer/app/src/main/java/me/suryaharahap/mynavigationdrawer/MainActivity.kt
@@ -11,12 +11,17 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import de.hdodenhof.circleimageview.CircleImageView
 import me.suryaharahap.mynavigationdrawer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+
+    private lateinit var profileCircleImageView: CircleImageView
+    private var profileImageUrl = "https://avatars.githubusercontent.com/u/115866512?v=4"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +37,11 @@ class MainActivity : AppCompatActivity() {
         }
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
+        profileCircleImageView = navView.getHeaderView(0).findViewById(R.id.imageView)
+        Glide.with(this)
+            .load(profileImageUrl)
+            .into(profileCircleImageView)
+
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
