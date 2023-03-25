ec16893925b6b1ce54c0a92d3e6081892ccf871c
suryaharahap <suryaharahap18@gmail.com>
2023-03-25 21:21:28 +0700

    Add: SubwayActivity

---
+++ MyNavigationDrawer/app/src/main/java/me/suryaharahap/mynavigationdrawer/SubwayActivity.kt
@@ -0,0 +1,11 @@
+package me.suryaharahap.mynavigationdrawer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SubwayActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_subway)
+    }
+}
