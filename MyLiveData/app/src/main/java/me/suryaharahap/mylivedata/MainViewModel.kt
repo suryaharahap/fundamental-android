dea0b33bb6e12e1446fade03785e574816955c24
suryaharahap <suryaharahap18@gmail.com>
2023-04-14 00:55:07 +0700

    Add: a LiveData object which will be subscribed by MainActivity MyViewModel app

--- MyLiveData/app/src/main/java/me/suryaharahap/mylivedata/MainViewModel.kt
+++ MyLiveData/app/src/main/java/me/suryaharahap/mylivedata/MainViewModel.kt
@@ -1,6 +1,8 @@
 package me.suryaharahap.mylivedata
 
 import android.os.SystemClock
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import java.util.Timer
 import java.util.TimerTask
@@ -12,13 +14,19 @@ class MainViewModel : ViewModel() {
     }
 
     private val mInitializeTime = SystemClock.elapsedRealtime()
+    private val mElapsedTime = MutableLiveData<Long?>()
 
     init {
         val timer = Timer()
         timer.scheduleAtFixedRate(object : TimerTask() {
             override fun run() {
                 val newValue = (SystemClock.elapsedRealtime() - mInitializeTime) / 1000
+                mElapsedTime.postValue(newValue)
             }
         }, ONE_SECOND.toLong(), ONE_SECOND.toLong())
+    }
+
+    fun getElapsedTime(): LiveData<Long?> {
+        return mElapsedTime
     }
 }
