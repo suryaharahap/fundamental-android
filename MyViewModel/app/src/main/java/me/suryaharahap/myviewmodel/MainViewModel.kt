d83597ec833f1bc8bfbfc05bec8390cedc6202ab
suryaharahap <suryaharahap18@gmail.com>
2023-04-13 13:30:42 +0700

    Add: class MainViewModel

---
+++ MyViewModel/app/src/main/java/me/suryaharahap/myviewmodel/MainViewModel.kt
@@ -0,0 +1,11 @@
+package me.suryaharahap.myviewmodel
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    var result = 0
+
+    fun calculate(width: String, height: String, length: String) {
+        result = width.toInt() * height.toInt() * length.toInt()
+    }
+}
